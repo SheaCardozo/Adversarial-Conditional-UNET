3323ecf3a72051a7ee2f40e94558c6aaea668824
SCARDOZO <sacardoz@uwaterloo.ca>
2022-01-04 17:17:55 -0500

    bash

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 mkdir CIFAR10
-tar -xzvf cifar-10-python.tar.gz CIFAR10/
+tar -xzvf cifar-10-python.tar.gz -C CIFAR10/
 
 git clone https://github.com/SheaCardozo/PyTorch_CIFAR10 PyTorch_CIFAR10/
 
