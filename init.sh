e6b58fed8e7b4eac7d16740050aaf171ed3a2093
SCARDOZO <sacardoz@uwaterloo.ca>
2022-01-04 17:14:48 -0500

    cifar

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 # Download CIFAR and pretrained models
 
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xzvf CIFAR10/cifar-10-python.tar.gz CIFAR10/
+mkdir CIFAR10
+tar -xzvf cifar-10-python.tar.gz CIFAR10/
 
 git clone https://github.com/SheaCardozo/PyTorch_CIFAR10 PyTorch_CIFAR10/
 
