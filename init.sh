04882ed248f7fd63478088f488f76100317cd126
SCARDOZO <sacardoz@uwaterloo.ca>
2022-01-04 08:45:33 -0500

    fix

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar -xzvf CIFAR10/cifar-10-python.tar.gz CIFAR10/
 
-git clone https://github.com/huyvnphan/PyTorch_CIFAR10 PyTorch_CIFAR10/
+git clone https://github.com/SheaCardozo/PyTorch_CIFAR10 PyTorch_CIFAR10/
 
 cd PyTorch_CIFAR10
 python train.py --download_weights 1
